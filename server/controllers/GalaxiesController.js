773aa5eda538438f8049f5ddfa140ad315a20c69
CyberDack <afntwrk.guyrpg@gmail.com>
2021-07-29 09:15:01 -0600

    can post galaxies

--- server/controllers/GalaxiesController.js
+++ server/controllers/GalaxiesController.js
@@ -1,12 +1,12 @@
 import { galaxiesService } from '../services/GalaxiesService'
-import BaseController from '..utils/BaseController'
+import BaseController from '../utils/BaseController'
 
 export class GalaxiesController extends BaseController {
   constructor() {
     super('api/galaxies')
     this.router
       .get('', this.getAll)
-      .get('/:id', this.getById)
+      // .get('/:id', this.getById)
       .post('', this.create)
   }
 
@@ -14,6 +14,15 @@ export class GalaxiesController extends BaseController {
     try {
       const galaxies = await galaxiesService.getAll(req.query)
       res.send(galaxies)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(req, res, next) {
+    try {
+      const galaxy = await galaxiesService.create(req.body)
+      res.send(galaxy)
     } catch (error) {
       next(error)
     }
