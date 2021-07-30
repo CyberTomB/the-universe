215e75b0d427c281fa1ccad064fb10915c43d655
CyberDack <afntwrk.guyrpg@gmail.com>
2021-07-29 21:28:40 -0600

    updated star contorller and service

---
+++ server/controllers/StarsController.js
@@ -0,0 +1,30 @@
+import { starsService } from '../services/StarsService'
+import BaseController from '../utils/BaseController'
+
+export class StarsController extends BaseController {
+  constructor() {
+    super('api/stars')
+    this.router
+      .get('', this.getAll)
+      // .get('/:id', this.getById)
+      .post('', this.create)
+  }
+
+  async getAll(req, res, next) {
+    try {
+      const stars = await starsService.getAll(req.query)
+      res.send(stars)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(req, res, next) {
+    try {
+      const star = await starsService.create(req.body)
+      res.send(star)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
--- server/models/Star.js
+++ server/models/Star.js
@@ -5,9 +5,16 @@ const ObjectId = Schema.Types.ObjectId
 const Star = new Schema(
   {
     name: { type: String, required: true },
-    galaxyId: { type: ObjectId, ref: 'Galaxy' }
+    galaxyId: { type: ObjectId, ref: 'Galaxy', required: true }
   },
   { timestamps: true, toJSON: { virtuals: true } }
 )
+
+Star.virtual('galaxy', {
+  localField: 'galaxyId',
+  ref: 'Galaxy',
+  foreignField: '_id',
+  justOne: true
+})
 
 export default Star
